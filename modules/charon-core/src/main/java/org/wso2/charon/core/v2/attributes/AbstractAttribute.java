53421cc519d9d137fb976d3ca635883f9f4778f2
Vindula Jayawardana <vindulamj@gmail.com>
2016-11-30 19:39:45 +0530

    Add Charon 3.0.0

---
+++ modules/charon-core/src/main/java/org/wso2/charon/core/v2/attributes/AbstractAttribute.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.charon.core.v2.attributes;
+
+import org.wso2.charon.core.v2.schema.SCIMDefinitions;
+/**
+ * This class abstracts out the common characteristics of different types of attributes defined in
+ * SCIM Core Schema Spec.
+*/
+public abstract class AbstractAttribute implements Attribute {
+
+    //unique identifier for the attribute
+    protected String uri;
+    //name of the attribute
+    protected String name;
+    //data type of the attribute
+    protected SCIMDefinitions.DataType type = null;
+    //Boolean value indicating the attribute's plurality.
+    protected Boolean multiValued;
+    //The attribute's human readable description
+    protected String description;
+    //A Boolean value that specifies if the attribute is required
+    protected Boolean required;
+    //A Boolean value that specifies if the String attribute is case sensitive
+    protected Boolean caseExact;
+    //A SCIM defined value that specifies if the attribute's mutability.
+    protected SCIMDefinitions.Mutability mutability;
+    //A SCIM defined value that specifies when the attribute's value need to be returned.
+    protected SCIMDefinitions.Returned returned;
+    //A SCIM defined value that specifies the uniqueness level of an attribute.
+    protected SCIMDefinitions.Uniqueness uniqueness;
+
+    public String getURI() {
+        return uri; }
+
+    public void setURI(String uri) {
+        this.uri = uri; }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SCIMDefinitions.DataType getType() {
+        return type;
+    }
+
+    public void setType(SCIMDefinitions.DataType type) {
+        this.type = type;
+    }
+
+    public Boolean getMultiValued() {
+        return multiValued;
+    }
+
+    public void setMultiValued(Boolean multiValued) {
+        this.multiValued = multiValued;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getRequired() {
+        return required;
+    }
+
+    public void setRequired(Boolean required) {
+        this.required = required;
+    }
+
+    public Boolean getCaseExact() {
+        return caseExact;
+    }
+
+    public void setCaseExact(Boolean caseExact) {
+        this.caseExact = caseExact;
+    }
+
+    public SCIMDefinitions.Mutability getMutability() {
+        return mutability;
+    }
+
+    public void setMutability(SCIMDefinitions.Mutability mutability) {
+        this.mutability = mutability;
+    }
+
+    public SCIMDefinitions.Returned getReturned() {
+        return returned; }
+
+    public void setReturned(SCIMDefinitions.Returned returned) {
+        this.returned = returned;
+    }
+
+    public SCIMDefinitions.Uniqueness getUniqueness() {
+        return uniqueness;
+    }
+
+    public void setUniqueness(SCIMDefinitions.Uniqueness uniqueness) {
+        this.uniqueness = uniqueness;
+    }
+
+}
---
+++ modules/charon-core/src/main/java/org/wso2/charon/core/v2/schema/SCIMResourceSchemaManager.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.charon.core.v2.schema;
+
+
+
+import org.wso2.charon.core.v2.config.SCIMUserSchemaExtensionBuilder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+* This is to check for extension schema for the user and buildTree a custom user schema with it.
+* Unless a extension is defined, core-user schema need to be returned.
+*/
+public class SCIMResourceSchemaManager {
+
+    private static SCIMResourceSchemaManager manager = new SCIMResourceSchemaManager();
+
+    public static SCIMResourceSchemaManager getInstance() {
+        return manager;
+    }
+
+    /*
+     * Return the SCIM User Resource Schema
+     *
+     * @return SCIMResourceTypeSchema
+     */
+    public SCIMResourceTypeSchema getUserResourceSchema() {
+
+
+        SCIMAttributeSchema schemaExtension = SCIMUserSchemaExtensionBuilder.getInstance().getExtensionSchema();
+        if (schemaExtension != null) {
+            return SCIMResourceTypeSchema.createSCIMResourceSchema(
+                    new ArrayList<String>(Arrays.asList(SCIMConstants.USER_CORE_SCHEMA_URI, schemaExtension.getURI())),
+                    SCIMSchemaDefinitions.ID, SCIMSchemaDefinitions.EXTERNAL_ID, SCIMSchemaDefinitions.META,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.USERNAME,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.NAME,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.DISPLAY_NAME,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.NICK_NAME,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.PROFILE_URL,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.TITLE,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.USER_TYPE,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.PREFERRED_LANGUAGE,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.LOCALE,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.TIME_ZONE,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.ACTIVE,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.PASSWORD,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.EMAILS,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.PHONE_NUMBERS,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.IMS,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.PHOTOS,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.ADDRESSES,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.GROUPS,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.ENTITLEMENTS,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.ROLES,
+                    SCIMSchemaDefinitions.SCIMUserSchemaDefinition.X509CERTIFICATES,
+                    schemaExtension);
+        }
+        return SCIMSchemaDefinitions.SCIM_USER_SCHEMA;
+    }
+
+    /*
+     * check whether the extension is enabled
+     *
+     * @return
+     */
+    public Boolean isExtensionSet() {
+        SCIMAttributeSchema schemaExtension = SCIMUserSchemaExtensionBuilder.getInstance().getExtensionSchema();
+        if (schemaExtension != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+     * return the extension name
+     *
+     * @return
+     */
+    public String getExtensionName() {
+        SCIMAttributeSchema schemaExtension = SCIMUserSchemaExtensionBuilder.getInstance().getExtensionSchema();
+        if (schemaExtension == null) {
+            return null;
+        }
+        return schemaExtension.getName();
+    }
+
+    /*
+     * return the extension uri
+     *
+     * @return
+     */
+    public String getExtensionURI() {
+        SCIMAttributeSchema schemaExtension = SCIMUserSchemaExtensionBuilder.getInstance().getExtensionSchema();
+        if (schemaExtension == null) {
+            return null;
+        }
+        return schemaExtension.getURI();
+    }
+
+    /*
+     * return the extension's required property
+     *
+     * @return
+     */
+    public boolean getExtensionRequired() {
+        SCIMAttributeSchema schemaExtension = SCIMUserSchemaExtensionBuilder.getInstance().getExtensionSchema();
+        if (schemaExtension == null) {
+            return false;
+        }
+        return schemaExtension.getRequired();
+    }
+
+    /*
+     * return service provider config resource schema
+     *
+     * @return
+     */
+    public SCIMResourceTypeSchema getServiceProviderConfigResourceSchema() {
+        return SCIMSchemaDefinitions.SCIM_SERVICE_PROVIDER_CONFIG_SCHEMA;
+    }
+
+    /*
+     * return group resource schema
+     *
+     * @return
+     */
+    public SCIMResourceTypeSchema getGroupResourceSchema() {
+        return SCIMSchemaDefinitions.SCIM_GROUP_SCHEMA;
+    }
+
+    /*
+     * return resource type resource schema
+     *
+     * @return
+     */
+    public SCIMResourceTypeSchema getResourceTypeResourceSchema() {
+        return SCIMSchemaDefinitions.SCIM_RESOURCE_TYPE_SCHEMA;
+    }
+
+}
