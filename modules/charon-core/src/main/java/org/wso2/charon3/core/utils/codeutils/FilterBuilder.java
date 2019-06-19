0a490b46d153ebeff75d0cf1145d470a83f97390
Captain-P-Goldfish <captain.p.goldfish@gmx.de>
2019-06-19 17:04:56 +0200

    Add filter methods for directly setting the attriubute names

--- modules/charon-core/src/main/java/org/wso2/charon3/core/utils/codeutils/FilterBuilder.java
+++ modules/charon-core/src/main/java/org/wso2/charon3/core/utils/codeutils/FilterBuilder.java
@@ -14,34 +14,33 @@ import java.util.Locale;
 import static org.wso2.charon3.core.schema.SCIMDefinitions.FilterOperation.PR;
 
 /**
- * author Pascal Knueppel <br>
- * created at: 22.03.2019 - 14:35 <br>
+ * author Pascal Knueppel <br> created at: 22.03.2019 - 14:35 <br>
  * <br>
- * can be used to build a SCIM filter expression
+ * can be used to build a SCIM filter expression.
  */
 public class FilterBuilder implements Serializable {
 
     private static final long serialVersionUID = 7231267409086617614L;
-    
+
     private static final String ESCAPE_STRING_LITERAL = "\"";
 
     /**
-     * the builder that is used to create the filter
+     * the builder that is used to create the filter.
      */
     private StringBuilder filter = new StringBuilder();
 
     /**
-     * counts the number of left parentheses
+     * counts the number of left parentheses.
      */
     private int leftParenthesisCount = 0;
 
     /**
-     * counts the number of right parentheses
+     * counts the number of right parentheses.
      */
     private int rightParenthesisCount = 0;
 
     /**
-     * if set to true a left parenthesis will be added before the next operation
+     * if set to true a left parenthesis will be added before the next operation.
      */
     private boolean addLeftParenthesis = false;
 
@@ -55,11 +54,14 @@ public class FilterBuilder implements Serializable {
 
     /**
      * adds an and expression to the filter - if the filter has no expressions yet the "and" is ignored and a simple
-     * filter expression is created
+     * filter expression is created.
      *
-     * @param attributeSchema the attribute schema that will describe the attribute-name of the expression
-     * @param operation       the operation if the value is equals, contains etc.
-     * @param value           the value to compare
+     * @param attributeSchema
+     *     the attribute schema that will describe the attribute-name of the expression
+     * @param operation
+     *     the operation if the value is equals, contains etc.
+     * @param value
+     *     the value to compare
      */
     public FilterBuilder and(AttributeSchema attributeSchema, SCIMDefinitions.FilterOperation operation, String value) {
         expression(attributeSchema, operation, value, SCIMDefinitions.FilterConcatenation.AND);
@@ -67,12 +69,31 @@ public class FilterBuilder implements Serializable {
     }
 
     /**
-     * adds an or expression to the filter - if the filter has no expressions yet the "or" is ignored and a simple
-     * filter expression is created
+     * adds an and expression to the filter - if the filter has no expressions yet the "and" is ignored and a simple
+     * filter expression is created.
      *
-     * @param attributeSchema the attribute schema that will describe the attribute-name of the expression
-     * @param operation       the operation if the value is equals, contains etc.
-     * @param value           the value to compare
+     * @param attributeNameOrUri
+     *     the attribute name of the field or the complete uri
+     * @param operation
+     *     the operation if the value is equals, contains etc.
+     * @param value
+     *     the value to compare
+     */
+    public FilterBuilder and(String attributeNameOrUri, SCIMDefinitions.FilterOperation operation, String value) {
+        expression(attributeNameOrUri, operation, value, SCIMDefinitions.FilterConcatenation.AND);
+        return this;
+    }
+
+    /**
+     * adds an or expression to the filter - if the filter has no expressions yet the "or" is ignored and a simple
+     * filter expression is created.
+     *
+     * @param attributeSchema
+     *     the attribute schema that will describe the attribute-name of the expression
+     * @param operation
+     *     the operation if the value is equals, contains etc.
+     * @param value
+     *     the value to compare
      */
     public FilterBuilder or(AttributeSchema attributeSchema, SCIMDefinitions.FilterOperation operation, String value) {
         expression(attributeSchema, operation, value, SCIMDefinitions.FilterConcatenation.OR);
@@ -80,11 +101,31 @@ public class FilterBuilder implements Serializable {
     }
 
     /**
-     * adds a not expression to the filter
+     * adds an or expression to the filter - if the filter has no expressions yet the "or" is ignored and a simple
+     * filter expression is created.
      *
-     * @param attributeSchema the attribute schema that will describe the attribute-name of the expression
-     * @param operation       the operation if the value is equals, contains etc.
-     * @param value           the value to compare
+     * @param attributeNameOrUri
+     *     the attribute name of the complete uri the attribute schema that will describe the attribute-name of the
+     *     expression
+     * @param operation
+     *     the operation if the value is equals, contains etc.
+     * @param value
+     *     the value to compare
+     */
+    public FilterBuilder or(String attributeNameOrUri, SCIMDefinitions.FilterOperation operation, String value) {
+        expression(attributeNameOrUri, operation, value, SCIMDefinitions.FilterConcatenation.OR);
+        return this;
+    }
+
+    /**
+     * adds a not expression to the filter.
+     *
+     * @param attributeSchema
+     *     the attribute schema that will describe the attribute-name of the expression
+     * @param operation
+     *     the operation if the value is equals, contains etc.
+     * @param value
+     *     the value to compare
      */
     public FilterBuilder not(AttributeSchema attributeSchema, SCIMDefinitions.FilterOperation operation, String value) {
         expression(attributeSchema, operation, value, SCIMDefinitions.FilterConcatenation.NOT);
@@ -92,7 +133,22 @@ public class FilterBuilder implements Serializable {
     }
 
     /**
-     * prepares for adding an opening parenthesis for the next operation call
+     * adds a not expression to the filter.
+     *
+     * @param attributeNameOrUri
+     *     the attribute name or the complete uri
+     * @param operation
+     *     the operation if the value is equals, contains etc.
+     * @param value
+     *     the value to compare
+     */
+    public FilterBuilder not(String attributeNameOrUri, SCIMDefinitions.FilterOperation operation, String value) {
+        expression(attributeNameOrUri, operation, value, SCIMDefinitions.FilterConcatenation.NOT);
+        return this;
+    }
+
+    /**
+     * prepares for adding an opening parenthesis for the next operation call.
      */
     public FilterBuilder leftParenthesis() {
         leftParenthesisCount++;
@@ -101,7 +157,7 @@ public class FilterBuilder implements Serializable {
     }
 
     /**
-     * adds a right parenthesis
+     * adds a right parenthesis.
      */
     public FilterBuilder rightParenthesis() {
         filter.append(')');
@@ -110,12 +166,16 @@ public class FilterBuilder implements Serializable {
     }
 
     /**
-     * builds the actual filter expression
+     * builds the actual filter expression.
      *
-     * @param attributeSchema the attribute schema that will describe the attribute-name of the expression
-     * @param operation       the operation if the value is equals, contains etc.
-     * @param value           the value to compare
-     * @param concatenation   the concatenation if the filter needs one yet
+     * @param attributeSchema
+     *     the attribute schema that will describe the attribute-name of the expression
+     * @param operation
+     *     the operation if the value is equals, contains etc.
+     * @param value
+     *     the value to compare
+     * @param concatenation
+     *     the concatenation if the filter needs one yet
      */
     private void expression(AttributeSchema attributeSchema, SCIMDefinitions.FilterOperation operation, String value,
                             SCIMDefinitions.FilterConcatenation concatenation) {
@@ -125,18 +185,48 @@ public class FilterBuilder implements Serializable {
             addLeftParenthesis = false;
         }
         filter.append(buildAttributeName(attributeSchema))
-                .append(' ')
-                .append(operation.name().toLowerCase(Locale.ENGLISH));
+            .append(' ')
+            .append(operation.name().toLowerCase(Locale.ENGLISH));
         if (!PR.equals(operation)) {
             filter.append(' ')
-                    .append(ESCAPE_STRING_LITERAL)
-                    .append(value)
-                    .append(ESCAPE_STRING_LITERAL);
+                .append(ESCAPE_STRING_LITERAL)
+                .append(value)
+                .append(ESCAPE_STRING_LITERAL);
         }
     }
 
     /**
-     * prepends a concatenation to the current position of the filter
+     * builds the actual filter expression.
+     *
+     * @param attributeNameOrUri
+     *     the attribute name or the complete uri
+     * @param operation
+     *     the operation if the value is equals, contains etc.
+     * @param value
+     *     the value to compare
+     * @param concatenation
+     *     the concatenation if the filter needs one yet
+     */
+    private void expression(String attributeNameOrUri, SCIMDefinitions.FilterOperation operation, String value,
+                            SCIMDefinitions.FilterConcatenation concatenation) {
+        prependConcatenation(concatenation);
+        if (addLeftParenthesis) {
+            filter.append('(');
+            addLeftParenthesis = false;
+        }
+        filter.append(attributeNameOrUri)
+            .append(' ')
+            .append(operation.name().toLowerCase(Locale.ENGLISH));
+        if (!PR.equals(operation)) {
+            filter.append(' ')
+                .append(ESCAPE_STRING_LITERAL)
+                .append(value)
+                .append(ESCAPE_STRING_LITERAL);
+        }
+    }
+
+    /**
+     * prepends a concatenation to the current position of the filter.
      */
     private void prependConcatenation(SCIMDefinitions.FilterConcatenation concatenation) {
         if (filter.length() != 0) {
@@ -149,10 +239,9 @@ public class FilterBuilder implements Serializable {
     /**
      * build the attribute name as it must be entered into the filter expression.<br>
      * <br>
-     * if the attribute is from the user or group core schema the simple attribute names will be used like
-     * "emails.type" or "userName". But if the attribute comes from an extension - discovered by the fact
-     * that the attribute uri does not start with one of the core schema uris - the full attribute uri is
-     * used as field-name
+     * if the attribute is from the user or group core schema the simple attribute names will be used like "emails.type"
+     * or "userName". But if the attribute comes from an extension - discovered by the fact that the attribute uri does
+     * not start with one of the core schema uris - the full attribute uri is used as field-name
      */
     private String buildAttributeName(AttributeSchema attributeSchema) {
         if (isSCIMAttributeSchema(attributeSchema)) {
@@ -173,18 +262,18 @@ public class FilterBuilder implements Serializable {
     }
 
     /**
-     * tells us if the given attribute is either a child of the user core schema or of the group core schema
+     * tells us if the given attribute is either a child of the user core schema or of the group core schema.
      */
     private boolean isAttributeFromCoreSchemas(AttributeSchema attributeSchema) {
         if (attributeSchema.getURI().startsWith(SCIMConstants.USER_CORE_SCHEMA_URI) ||
-                attributeSchema.getURI().startsWith(SCIMConstants.GROUP_CORE_SCHEMA_URI)) {
+            attributeSchema.getURI().startsWith(SCIMConstants.GROUP_CORE_SCHEMA_URI)) {
             return true;
         }
         return false;
     }
 
     /**
-     * rells us if the attribute is of type {@link SCIMAttributeSchema}
+     * rells us if the attribute is of type {@link SCIMAttributeSchema}.
      */
     private boolean isSCIMAttributeSchema(AttributeSchema attributeSchema) {
         return attributeSchema instanceof SCIMAttributeSchema;
@@ -198,7 +287,7 @@ public class FilterBuilder implements Serializable {
             LambdaExceptionUtils.rethrowConsumer(ex -> {
                 throw (Exception) ex;
             }).accept(new InternalErrorException("invalid filter expression. You inserted " + leftParenthesisCount
-                    + " left parentheses and " + rightParenthesisCount + " right parentheses"));
+                + " left parentheses and " + rightParenthesisCount + " right parentheses"));
         }
         return StringUtils.stripToNull(filter.toString());
     }
--- modules/charon-core/src/test/java/org/wso2/charon3/core/utils/codeutils/FilterBuilderTest.java
+++ modules/charon-core/src/test/java/org/wso2/charon3/core/utils/codeutils/FilterBuilderTest.java
@@ -20,14 +20,14 @@ import static org.wso2.charon3.core.schema.SCIMSchemaDefinitions.SCIMUserSchemaD
 import static org.wso2.charon3.core.schema.SCIMSchemaDefinitions.SCIMUserSchemaDefinition.USERNAME;
 
 /**
- * author Pascal Knueppel <br>
- * created at: 22.03.2019 - 15:33 <br>
+ * author Pascal Knueppel <br> created at: 22.03.2019 - 15:33 <br>
  * <br>
+ * .
  */
 public class FilterBuilderTest {
 
     /**
-     * tests that the {@link FilterBuilder} works correctly
+     * tests that the {@link FilterBuilder} works correctly.
      */
     @Test
     public void testFilterBuilder() {
@@ -48,23 +48,65 @@ public class FilterBuilderTest {
         Assertions.assertEquals(MANAGER_URI + " co \"" + randomValue + "\"", filter);
 
         filter = FilterBuilder.builder().and(MANAGER, CO, randomValue)
-                .and(USERNAME, SW, randomValue).build();
+            .and(USERNAME, SW, randomValue).build();
         Assertions.assertEquals(MANAGER_URI + " co \"" + randomValue + "\" and userName sw \""
-                + randomValue + "\"", filter);
+            + randomValue + "\"", filter);
 
         filter = FilterBuilder.builder().and(EMAIL_VALUE, EQ, randomValue)
-                .and(USERNAME, SW, randomValue)
-                .leftParenthesis()
-                .or(NICK_NAME, EQ, randomValue)
-                .or(GIVEN_NAME, EQ, randomValue)
-                .rightParenthesis()
-                .and(FAMILY_NAME, EQ, randomValue).build();
+            .and(USERNAME, SW, randomValue)
+            .leftParenthesis()
+            .or(NICK_NAME, EQ, randomValue)
+            .or(GIVEN_NAME, EQ, randomValue)
+            .rightParenthesis()
+            .and(FAMILY_NAME, EQ, randomValue).build();
         Assertions.assertEquals(
-                "emails.value eq \"" + randomValue + "\" and "
-                        + "userName sw \"" + randomValue + "\" or "
-                        + "(nickName eq \"" + randomValue + "\" or "
-                        + "name.givenName eq \"" + randomValue + "\") and "
-                        + "name.familyName eq \"" + randomValue + "\""
-                , filter);
+            "emails.value eq \"" + randomValue + "\" and "
+                + "userName sw \"" + randomValue + "\" or "
+                + "(nickName eq \"" + randomValue + "\" or "
+                + "name.givenName eq \"" + randomValue + "\") and "
+                + "name.familyName eq \"" + randomValue + "\""
+            , filter);
+    }
+
+    /**
+     * shows that the filter-builder does also work if the fieldnames are added directly.
+     */
+    @Test
+    public void testFilterBuilderWithStrings() {
+        final String randomValue = UUID.randomUUID().toString();
+        String filter = FilterBuilder.builder().and("emails.display", EQ, randomValue).build();
+        Assertions.assertEquals("emails.display eq \"" + randomValue + "\"", filter);
+
+        filter = FilterBuilder.builder().and("emails.type", EQ, randomValue).build();
+        Assertions.assertEquals("emails.type eq \"" + randomValue + "\"", filter);
+
+        filter = FilterBuilder.builder().and("emails.type", CO, randomValue).build();
+        Assertions.assertEquals("emails.type co \"" + randomValue + "\"", filter);
+
+        filter = FilterBuilder.builder().and("phoneNumbers.display", CO, randomValue).build();
+        Assertions.assertEquals("phoneNumbers.display co \"" + randomValue + "\"", filter);
+
+        filter = FilterBuilder.builder().and("manager", CO, randomValue).build();
+        Assertions.assertEquals("manager co \"" + randomValue + "\"", filter);
+
+        filter = FilterBuilder.builder().and("manager", CO, randomValue)
+            .and("userName", SW, randomValue).build();
+        Assertions.assertEquals("manager co \"" + randomValue + "\" and userName sw \""
+            + randomValue + "\"", filter);
+
+        filter = FilterBuilder.builder().and("emails.value", EQ, randomValue)
+            .and("userName", SW, randomValue)
+            .leftParenthesis()
+            .or("nickName", EQ, randomValue)
+            .or("name.givenName", EQ, randomValue)
+            .rightParenthesis()
+            .and("name.familyName", EQ, randomValue).build();
+        Assertions.assertEquals(
+            "emails.value eq \"" + randomValue + "\" and "
+                + "userName sw \"" + randomValue + "\" or "
+                + "(nickName eq \"" + randomValue + "\" or "
+                + "name.givenName eq \"" + randomValue + "\") and "
+                + "name.familyName eq \"" + randomValue + "\""
+            , filter);
     }
 }
