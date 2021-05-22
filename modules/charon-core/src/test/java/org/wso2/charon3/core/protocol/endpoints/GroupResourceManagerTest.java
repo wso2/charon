49b5730d9c806f6ab8311c2c89f43ee5089c0180
Ravindu De Silva <ravindud@scorelab.org>
2021-05-22 10:50:18 +0530

    Add unit tests for GroupResourceManager (code review issues fixed wso2 #5)

--- modules/charon-core/src/test/java/org/wso2/charon3/core/protocol/endpoints/GroupResourceManagerTest.java
+++ modules/charon-core/src/test/java/org/wso2/charon3/core/protocol/endpoints/GroupResourceManagerTest.java
@@ -22,6 +22,7 @@ import org.json.JSONObject;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.testng.PowerMockTestCase;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -118,6 +119,7 @@ public class GroupResourceManagerTest extends PowerMockTestCase {
         userManager = mock(UserManager.class);
     }
 
+    @AfterMethod
     public void tearDown() {
 
     }
