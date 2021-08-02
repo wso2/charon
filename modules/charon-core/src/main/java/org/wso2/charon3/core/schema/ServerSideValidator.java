e7f3292b83aceac38dab4a43a54ee2c660c3f4bf
GANGANI <gcariyarathne.15@cse.mrt.ac.lk>
2021-08-02 10:40:32 +0530

    Fix user listing with required attributes

--- modules/charon-core/src/main/java/org/wso2/charon3/core/schema/ServerSideValidator.java
+++ modules/charon-core/src/main/java/org/wso2/charon3/core/schema/ServerSideValidator.java
@@ -110,7 +110,7 @@ public class ServerSideValidator extends AbstractValidator {
                                                                  reuqestedAttributes,
                                                          String requestedExcludingAttributes)
             throws BadRequestException, CharonException {
-        validateSCIMObjectForRequiredAttributes(scimObject, resourceSchema);
+
         validateReturnedAttributes(scimObject, reuqestedAttributes, requestedExcludingAttributes);
     }
 
@@ -128,7 +128,7 @@ public class ServerSideValidator extends AbstractValidator {
                                                    SCIMResourceTypeSchema resourceSchema, String reuqestedAttributes,
                                                    String requestedExcludingAttributes)
             throws BadRequestException, CharonException {
-        validateSCIMObjectForRequiredAttributes(scimObject, resourceSchema);
+
         validateReturnedAttributes(scimObject, reuqestedAttributes, requestedExcludingAttributes);
         validateSchemaList(scimObject, resourceSchema);
     }
