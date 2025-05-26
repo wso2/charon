97bff00195c6fe336cc7ce9be1eb9ea2033f3d2d
asha15 <165079T@uom.lk>
2025-05-26 22:45:02 +0530

    Add SCIM2 Role API v3 support

--- modules/charon-core/src/main/java/org/wso2/charon3/core/protocol/endpoints/ResourceManager.java
+++ modules/charon-core/src/main/java/org/wso2/charon3/core/protocol/endpoints/ResourceManager.java
@@ -352,4 +352,101 @@ public interface ResourceManager {
         return new SCIMResponse(ResponseCodeConstants.CODE_NOT_IMPLEMENTED, ResponseCodeConstants.DESC_NOT_IMPLEMENTED,
                 Collections.emptyMap());
     }
+
+    /**
+     * POST method to create a new role in roleV2 model.
+     *
+     * @param postRequest Post request.
+     * @param roleManager RoleV2 manager.
+     * @return SCIMResponse.
+     */
+    default SCIMResponse createRoleMeta(String postRequest, RoleV2Manager roleManager) {
+
+        return new SCIMResponse(ResponseCodeConstants.CODE_NOT_IMPLEMENTED, ResponseCodeConstants.DESC_NOT_IMPLEMENTED,
+                Collections.emptyMap());
+    }
+
+    /**
+     * PUT method To update a role by giving entire attributes set.
+     *
+     * @param id          Resource id.
+     * @param putRequest  Put request.
+     * @param roleManager RoleV2 manager.
+     * @return SCIMResponse.
+     */
+    default SCIMResponse updateWithPUTRoleMeta(String id, String putRequest, RoleV2Manager roleManager) {
+
+        return new SCIMResponse(ResponseCodeConstants.CODE_NOT_IMPLEMENTED, ResponseCodeConstants.DESC_NOT_IMPLEMENTED,
+                Collections.emptyMap());
+    }
+
+    /**
+     * PATCH method to partially updates a role.
+     *
+     * @param id           Resource id.
+     * @param patchRequest Patch request.
+     * @param roleManager  RoleV2 manager.
+     * @return SCIMResponse.
+     */
+    default SCIMResponse updateWithPATCHRoleMeta(String id, String patchRequest, RoleV2Manager roleManager) {
+
+        return new SCIMResponse(ResponseCodeConstants.CODE_NOT_IMPLEMENTED, ResponseCodeConstants.DESC_NOT_IMPLEMENTED,
+                Collections.emptyMap());
+    }
+
+    /**
+     * PUT method to update a Role by giving the User list.
+     *
+     * @param id          Resource id.
+     * @param putRequest  Put request.
+     * @param roleManager RoleV3 manager.
+     * @return SCIMResponse.
+     */
+    default SCIMResponse updateUsersWithPUTRole(String id, String putRequest, RoleV2Manager roleManager) {
+
+        return new SCIMResponse(ResponseCodeConstants.CODE_NOT_IMPLEMENTED, ResponseCodeConstants.DESC_NOT_IMPLEMENTED,
+                Collections.emptyMap());
+    }
+
+    /**
+     * PATCH method to update a user with roleV2 model.
+     *
+     * @param id           Resource id.
+     * @param patchRequest Patch request.
+     * @param roleManager  RoleV3 manager.
+     * @return SCIMResponse.
+     */
+    default SCIMResponse updateUsersWithPATCHRole(String id, String patchRequest, RoleV2Manager roleManager) {
+
+        return new SCIMResponse(ResponseCodeConstants.CODE_NOT_IMPLEMENTED, ResponseCodeConstants.DESC_NOT_IMPLEMENTED,
+                Collections.emptyMap());
+    }
+
+    /**
+     * PUT method tp update a Role by giving the Group list.
+     *
+     * @param id          Resource id.
+     * @param putRequest  Put request.
+     * @param roleManager RoleV3 manager.
+     * @return SCIMResponse.
+     */
+    default SCIMResponse updateGroupsWithPUTRole(String id, String putRequest, RoleV2Manager roleManager) {
+
+        return new SCIMResponse(ResponseCodeConstants.CODE_NOT_IMPLEMENTED, ResponseCodeConstants.DESC_NOT_IMPLEMENTED,
+                Collections.emptyMap());
+    }
+
+    /**
+     * PATCH method to update a groups with roleV3 model.
+     *
+     * @param id           Resource id.
+     * @param patchRequest Patch request.
+     * @param roleManager  RoleV3 manager.
+     * @return SCIMResponse.
+     */
+    default SCIMResponse updateGroupsWithPATCHRole(String id, String patchRequest, RoleV2Manager roleManager) {
+
+        return new SCIMResponse(ResponseCodeConstants.CODE_NOT_IMPLEMENTED, ResponseCodeConstants.DESC_NOT_IMPLEMENTED,
+                Collections.emptyMap());
+    }
 }
