0f6c91ed76e54dbc9a3ad37cc8a75e1331cd02fc
Malith-19 <malithdilshan91@gmail.com>
2025-06-24 09:50:32 +0530

    Handle accepted error response in RoleResourceV2Manager in role creation.

--- modules/charon-core/src/main/java/org/wso2/charon3/core/protocol/endpoints/RoleResourceV2Manager.java
+++ modules/charon-core/src/main/java/org/wso2/charon3/core/protocol/endpoints/RoleResourceV2Manager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ * Copyright (c) 2023-2025, WSO2 LLC. (http://www.wso2.com).
  *
  * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -136,6 +136,9 @@ public class RoleResourceV2Manager extends AbstractResourceManager {
 
         } catch (InternalErrorException | BadRequestException | ConflictException | CharonException | NotFoundException
                  | NotImplementedException e) {
+            if (e.getStatus() == ResponseCodeConstants.CODE_ACCEPTED) {
+                return new SCIMResponse(ResponseCodeConstants.CODE_ACCEPTED, null, null);
+            }
             return encodeSCIMException(e);
         }
     }
